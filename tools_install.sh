832e314ca5c65a076121a821cce8f1927b61be5f
Alecto <128353161+AlectoSystem@users.noreply.github.com>
2023-10-28 10:13:45 +0900

    Add files via upload

---
+++ tools_install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+apt update
+apt upgrade
+
+apt install tesseract-ocr
+apt install libtesseract-dev
+
+apt install tesseract-ocr-jpn tesseract-ocr-jpn-vert
+apt install tesseract-ocr-script-jpan tesseract-ocr-script-jpan-vert
+
